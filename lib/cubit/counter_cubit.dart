6f27f6f141f551639e0fa567a37cafa2368849b3
m7mdragab <mr341349@gmail.com>
2024-10-29 19:03:59 +0300

    function in cubit

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -3,4 +3,17 @@ import 'package:points_counter/cubit/counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit(super.initialState);
+  int teamAPoints = 0;
+
+  int teamBPoints = 0;
+
+  void teamAIncrement({required String team, required int bottomNumber}) {
+    if (team == 'A') {
+      teamAPoints += bottomNumber;
+      emit(CounterAIncrementState());
+    } else {
+      teamBPoints += bottomNumber;
+      emit(CounterBIncrementState());
+    }
+  }
 }
